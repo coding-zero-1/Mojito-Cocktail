814874b5a260ff08cf8aeb8e39edd161ef81deeb
coding-zero-I <yashsangwan4700@gmail.com>
2025-07-03 10:52:37 +0530

    added project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ScrollTrigger,SplitText } from "gsap/all"
+import gsap from "gsap"
+gsap.registerPlugin(ScrollTrigger,SplitText)
+function App() {
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
